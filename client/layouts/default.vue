ef4464988ba18ed61220f24653494a55017ff61f
howell5 <howell95@outlook.com>
2024-11-22 22:21:17 +0800

    feat: layouts

---
+++ client/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header class="fixed top-0 inset-x-0 w-full h-80">
+      <h1>header</h1>
+    </header>
+    <main></main>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ client/pages/i18n/[project]/[slug].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>i18n</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const project = route.params.project
+const slug = route.params.slug
+
+console.log(project, slug)
+</script>
+
+<style scoped></style>
