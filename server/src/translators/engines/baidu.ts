243e2e0b8077bbeb994bf1983f5d2c6f6fceb63d
howell5 <howell95@outlook.com>
2025-01-16 00:00:06 +0800

    feat: add baidu

--- server/src/translators/engines/baidu.ts
+++ server/src/translators/engines/baidu.ts
@@ -1,8 +1,46 @@
 import BaseTranslator, { TranslateOptions } from './base'
+import { ofetch } from 'ofetch'
+import crypto from 'crypto'
+
+interface BaiduRequest {
+  from: string
+  to: string
+  q: string
+  appid: string
+  salt: string
+  sign: string
+}
 
 export class BaiduTranslator extends BaseTranslator {
+  private baiduHttpPath = 'https://fanyi-api.baidu.com/api/trans/vip/translate'
+
+  private appid = '20240325002004737'
+
+  private secret = '6WWkRg0QPtJiw4jDbZte'
+
+  private genSign(q: string, salt: string) {
+    const sign = this.appid + q + salt + this.secret
+    return crypto.createHash('md5').update(sign).digest('hex')
+  }
+
   async translate(options: TranslateOptions) {
-    return options.text
+    const salt = Date.now().toString()
+    const sign = this.genSign(options.text, salt)
+    const params: BaiduRequest = {
+      from: options.from,
+      to: options.to,
+      q: options.text,
+      appid: this.appid,
+      salt,
+      sign,
+    }
+
+    const res = await ofetch(this.baiduHttpPath, {
+      method: 'GET',
+      params,
+    })
+
+    return res?.trans_result?.[0].dst
   }
 }
 
--- server/src/translators/engines/google.ts
+++ server/src/translators/engines/google.ts
@@ -1,8 +1,33 @@
 import BaseTranslator, { TranslateOptions } from './base'
+import { ofetch } from 'ofetch'
+
+interface GoogleRequest {
+  q: string
+  target: string
+  source?: string
+  key: string
+}
 
 export class GoogleTranslator extends BaseTranslator {
+  private googleHttpPath =
+    'https://translation.googleapis.com/language/translate/v2'
+
+  private apiKey = 'AIzaSyD86-59-80-90-59-80-90-59-80-90-59-80-90'
+
   async translate(options: TranslateOptions) {
-    return options.text
+    const params: GoogleRequest = {
+      q: options.text,
+      target: options.to,
+      source: options.from,
+      key: this.apiKey,
+    }
+
+    const res = await ofetch(this.googleHttpPath, {
+      method: 'GET',
+      params,
+    })
+
+    return res?.data?.translations?.[0]?.translatedText
   }
 }
 
